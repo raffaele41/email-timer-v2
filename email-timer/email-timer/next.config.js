5dee34440e0ff4fd1ceaf4612876adda94ddcf8a
raffaele41 <60689722+raffaele41@users.noreply.github.com>
2026-03-10 17:12:56 +0100

    Update next.config.js

--- email-timer/email-timer/next.config.js
+++ email-timer/email-timer/next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    outputFileTracingIncludes: {
+      './app/api/countdown/route': ['./email-timer/email-timer/*.ttf'],
+    },
+  },
   webpack: (config, { isServer }) => {
     if (isServer) {
       config.externals = [...(config.externals || []), '@napi-rs/canvas']
